ce17ae5fbb8850e460e8db64f8c59fd539c233ae
Kuma <kuma@insiberia.net>
2026-03-30 19:48:37 -0300

    implements sorta a spa engine

--- internal/routes.go
+++ internal/routes.go
@@ -4,14 +4,32 @@ package internal
 import (
 	http "net/http"
 
+	about "meryl.moe/internal/modules/about"
+	articles "meryl.moe/internal/modules/articles"
+	cyberia "meryl.moe/internal/modules/cyberia"
 	home "meryl.moe/internal/modules/home"
+	notfound "meryl.moe/internal/modules/notfound"
+	tools "meryl.moe/internal/modules/tools"
 )
 
-func (server *Server) RegisterRoutes(homeHandler *home.Handler) {
+func (server *Server) RegisterRoutes(
+	homeHandler *home.Handler,
+	aboutHandler *about.Handler,
+	articlesHandler *articles.Handler,
+	toolsHandler *tools.Handler,
+	cyberiaHandler *cyberia.Handler,
+	notFoundHandler *notfound.Handler,
+) {
 	fileServer := http.FileServer(fileOnlyFS{fileSystem: http.Dir("static")})
 	server.router.Handle("/static/*", http.StripPrefix("/static/", fileServer))
 
 	server.router.Get("/", homeHandler.Index)
+	server.router.Get("/about", aboutHandler.Index)
+	server.router.Get("/articles", articlesHandler.Index)
+	server.router.Get("/tools", toolsHandler.Index)
+	server.router.Get("/cyberia", cyberiaHandler.Index)
+
+	server.router.NotFound(notFoundHandler.Index)
 
 	// TODO: testing endpoint, remove later
 	server.router.Get("/api/lain", func(writer http.ResponseWriter, request *http.Request) {
