ef5c1d496efa58c598cf111c3f9049d13d4f7408
Kuma <kuma@insiberia.net>
2026-04-04 02:02:01 -0300

    introduces vim motions, and refactors gutter, cursor and relay editor

---
+++ static/js/codec.js
@@ -0,0 +1,33 @@
+export async function compress(text) {
+	const stream = new CompressionStream('deflate-raw');
+	const writer = stream.writable.getWriter();
+
+	writer.write(new TextEncoder().encode(text));
+	writer.close();
+
+	const buffer = await new Response(stream.readable).arrayBuffer();
+
+	return toBase64url(new Uint8Array(buffer));
+}
+
+export async function decompress(base64url) {
+	const stream = new DecompressionStream('deflate-raw');
+	const writer = stream.writable.getWriter();
+
+	writer.write(fromBase64url(base64url));
+	writer.close();
+
+	return new Response(stream.readable).text();
+}
+
+function toBase64url(bytes) {
+	return btoa(Array.from(bytes, b => String.fromCharCode(b)).join(''))
+		.replaceAll('+', '-').replaceAll('/', '_').replaceAll('=', '');
+}
+
+function fromBase64url(base64url) {
+	return Uint8Array.from(
+		atob(base64url.replaceAll('-', '+').replaceAll('_', '/')),
+		c => c.charCodeAt(0),
+	);
+}
