7af22fe188faaec59d56a892b9f7f4bced7ab06c
Kuma <kuma@insiberia.net>
2026-04-05 14:02:22 -0300

    centralizes styles, refactors page transition script

--- internal/platform/middleware/security.go
+++ internal/platform/middleware/security.go
@@ -11,7 +11,7 @@ func Security(next http.Handler) http.Handler {
 		writer.Header().Set("X-Frame-Options", "DENY")
 		writer.Header().Set(
 			"Content-Security-Policy",
-			"default-src 'self'; style-src 'self' 'unsafe-hashes' 'sha256-faU7yAF8NxuMTNEwVmBz+VcYeIoBQ2EMHW3WaVxCvnk=' 'sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU='; script-src 'self'; img-src 'self'",
+			"default-src 'self'; style-src 'self' 'unsafe-hashes' 'sha256-faU7yAF8NxuMTNEwVmBz+VcYeIoBQ2EMHW3WaVxCvnk=' 'sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=' 'sha256-Z7wqRMsXrTzenFN+Xlsq0Ot702MqTqY52FZleRnUZkc=' 'sha256-C5nbf+LODEXP/7nHS3i3vf4uP0YK2m0n0es7V3eXNlU=' 'sha256-hp7L9m8vZle+uJzcJz5TLXdNUweRTJfazK7dAfdIDvc='; script-src 'self'; img-src 'self'",
 		)
 
 		next.ServeHTTP(writer, request)
